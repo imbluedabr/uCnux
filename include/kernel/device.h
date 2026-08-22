42152e60240d6678c1b6296f8364713cf24e75d7
imbluedabr <baldergroen@gmail.com>
2026-08-22 18:25:44 +0200

    feat(device): device model rewrite

--- include/kernel/device.h
+++ include/kernel/device.h
@@ -2,59 +2,97 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <uapi/sys/types.h>
-#include <kernel/waiter.h>
 
-//maximum of io_request's that can be allocated at one time
-#define IO_REQUEST_MAX_MEMUSAGE 8
+struct file;
+struct device;
+struct bus_device;
 
-struct io_request {
-    void* buffer;
-    ssize_t offset;
-    uint32_t count  : 24;
-    uint8_t op      : 1; //op: read = 0, write = 1
-    uint8_t status  : 1; //status: pending = 0, done = 1
-    uint8_t res0    : 6; //reserved
-    waiter_t waiter;     //process waiting for the request to complete
+enum bus_types : uint8_t {
+    BUS_NONE,
+    BUS_MMIO,
+    BUS_I2C,
+    BUS_SPI,
+    BUS_UART
 };
 
-struct device;
+//bus device descriptors, contains info about the location and type of the device on the bus
+struct mmio_bus_desc {
+    const void* base;
+    uint16_t vendor_id;
+    uint8_t major;
+    uint8_t irq;
+};
+
+struct i2c_bus_desc {
+    uint8_t major;
+    uint8_t address;
+};
 
 struct device_driver {
-    struct device* (*create)(void* descriptor);
-    int (*destroy)(struct device* dev);
-    int (*ioctl)(struct device* dev, int op, void* arg);
-    int (*readb)(struct device* dev);
-    int (*writeb)(struct device* dev, char val);
-    void (*update)(struct device* dev);
-    struct device* instances; //linked list of device instances
+    struct device* (*probe)(struct bus_device* parent, const void* descriptor);
+    int (*remove)(struct device* dev);
     const char* name;
+    struct device* instances; //linked list of device instances
     uint8_t instance_count;
+    enum bus_types bus_accept; //bus type this driver attaches to
+    enum bus_types bus_type; //bus type of this driver implements(if any)
 };
 
-#define DEVICE_IOQUEUE_LEN 8
-#define DEVICE_IOQUEUE_MSK (DEVICE_IOQUEUE_LEN - 1)
+struct dev_ops {
+    ssize_t (*read)(struct file* f, void* buff, size_t count);
+    ssize_t (*write)(struct file* f, const void* buff, size_t count);
+    off_t (*lseek)(struct file* f, off_t offset, int whence);
+    int (*ioctl)(struct file* f, int cmd, void* arg);
+    int (*open)(struct device* dev, struct file* f);
+    int (*release)(struct device* dev, struct file* f);
+};
+
 struct device {
     struct device_driver* driver;
+    struct dev_ops* ops;
     struct device* next; //next device in the linked list of devices
-    struct io_request* io_queue[DEVICE_IOQUEUE_LEN];
-    uint8_t io_queue_head;
-    uint8_t io_queue_tail;
-    uint8_t impl; //for selecting backends
+    struct bus_device* parent;   
     uint8_t minor; //minor number of this instance
+};
+
+#define BUS_MAX_DRIVERS 16
+struct bus_driver {
+    struct device_driver base;
+    uint8_t registered_drivers[BUS_MAX_DRIVERS];
+    int registered_drivers_count;
+};
+
+struct bus_ops {
+    int (*enumerate)(struct bus_device* bus);
+    struct device* (*probe)(struct bus_device* bus, const void* desc);
+
+    //i2c
+    int (*i2c_tx)(struct bus_device* bus, uint8_t addr, uint8_t* buff, int count);
+    int (*i2c_rx)(struct bus_device* bus, uint8_t addr, uint8_t* buff, int count);
+
+    //uart
+    int (*uart_tx)(struct bus_device* bus, uint8_t* buff);
+    int (*uart_rx)(struct bus_device* bus, uint8_t* buff);
+    
+    //gpio
+    int (*gpio_mode_set)(struct bus_device* bus, int mask);
+    int (*gpio_mode_clr)(struct bus_device* bus, int mask);
+    int (*gpio_port_set)(struct bus_device* bus, int mask);
+    int (*gpio_port_clr)(struct bus_device* bus, int mask);
+    int (*gpio_port_get)(struct bus_device* bus, int* buff);
+};
+
+struct bus_device {
+    struct device base;
+    struct bus_ops* bus_ops;
 };
 
 #define DRIVER_TABLE_LEN 16
 extern struct device_driver* driver_table[DRIVER_TABLE_LEN];
 
 void device_init();
-struct device* device_create(dev_t* devno, uint8_t major, const void* desc);
-int device_request_io(struct device* dev, struct io_request* req); //enqueue an io request in the io queue of a device
-struct io_request* device_dequeue_request(struct device* dev); //dequeue the top request from the queue
-struct io_request* device_peek_request(struct device* dev); //get the top request without removing it from the queue
-void device_finish_request(struct device* dev, ssize_t bytes_transfered); //finish a device request obtained via peek
-ssize_t device_write(struct device* dev, void* buffer, size_t count, off_t offset); //blocking write to device
-ssize_t device_read(struct device* dev, void* buffer, size_t count, off_t offset);
+int register_driver(uint8_t major, struct device_driver* drv);
+int bus_add_driver(struct bus_driver* drv, uint8_t major);
+struct device* device_probe(uint8_t major, struct bus_device* parent, const void* desc);
 struct device* device_lookup(dev_t devno);
-void device_global_update();
-
 
--- kernel/device.c
+++ kernel/device.c
@@ -1,6 +1,5 @@
 #include <kernel/device.h>
-#include <kernel/interrupt.h>
-#include <kernel/proc.h>
+#include <uapi/sys/errno.h>
 
 struct device_driver* driver_table[DRIVER_TABLE_LEN];
 
@@ -11,138 +10,51 @@ void device_init()
     }
 }
 
-struct device* device_create(dev_t* devno, uint8_t major, const void* desc)
+int bus_add_driver(struct bus_driver* drv, uint8_t major)
 {
-    struct device_driver* drv = driver_table[major];
-    if (drv == NULL) {
-        return NULL;
-    }
-    struct device* dev = drv->create((void*) desc);
-    if (dev == NULL) {
-        return NULL;
-    }
-    dev->next = drv->instances;
-    drv->instances = dev;
-    dev->minor = drv->instance_count++;
-    *devno = MKDEV(major, dev->minor);
-    return dev;
-}
-
-int device_request_io(struct device* dev, struct io_request* req)
-{
-    int irq = disable_interrupts();
-    uint8_t tmp = dev->io_queue_head;
-    if (dev->io_queue[tmp] != NULL) {
-        enable_interrupts(irq);
-        return -1; //no space left
-    }
-    dev->io_queue[tmp] = req;
-    dev->io_queue_head = (tmp + 1) & DEVICE_IOQUEUE_MSK;
-
-    enable_interrupts(irq);
+    if (drv->registered_drivers_count > BUS_MAX_DRIVERS) return -EAGAIN;
+    drv->registered_drivers[drv->registered_drivers_count++] = major;
     return 0;
 }
 
-struct io_request* device_dequeue_request(struct device* dev)
+int register_driver(uint8_t major, struct device_driver* drv)
 {
-    int irq = disable_interrupts();
-    uint8_t tmp = dev->io_queue_tail;
-    if (dev->io_queue[tmp] == NULL) {
-        enable_interrupts(irq);
-        return NULL; //there is nothing in the buffer
+    driver_table[major] = drv;
+    for (int i = 0; i < DRIVER_TABLE_LEN; i++) {
+        struct device_driver* bus = driver_table[i];
+        if (bus) {
+            if (bus->bus_type == drv->bus_accept) {
+                if (bus_add_driver((struct bus_driver*) bus, major) < 0)
+                    return -EAGAIN;
+            }
+        }
     }
-    dev->io_queue_tail = (tmp + 1) & DEVICE_IOQUEUE_MSK;
-    struct io_request* req = dev->io_queue[tmp];
-    dev->io_queue[tmp] = NULL;
-    enable_interrupts(irq);
-    return req;
+    return 0;
 }
 
-struct io_request* device_peek_request(struct device* dev)
+//TODO: add spinlock to probe 
+struct device* device_probe(uint8_t major, struct bus_device* parent, const void* desc)
 {
-    int irq = disable_interrupts();
-    uint8_t tmp = dev->io_queue_tail;
-    struct io_request* req = dev->io_queue[tmp]; //if the queue is empty this will just return NULL
-    enable_interrupts(irq);
-    return req;
+    struct device_driver* drv = driver_table[major];
+    if (!drv) return NULL;
+
+    struct device* dev = drv->probe(parent, desc);
+    if (!dev) return NULL;
+    dev->minor = drv->instance_count++;
+    dev->next = drv->instances;
+    drv->instances = dev;
+    return dev;
 }
 
-void device_finish_request(struct device* dev, ssize_t bytes_transfered) //finish a device request obtained via peek
-{
-    struct io_request* req = device_peek_request(dev);
-    req->offset = bytes_transfered;
-    req->status = 1;
-    struct proc* p = waiter_pop(&req->waiter);
-    if (p) {
-        proc_unblock_process(p);
-    }
-    device_dequeue_request(dev);
-}
-
-ssize_t device_write(struct device* dev, void* buffer, size_t count, off_t offset) //blocking write to device
-{
-    struct io_request req = {
-        .buffer = buffer,
-        .offset = offset,
-        .count = count,
-        .op = 1
-    };
-    waiter_push(&req.waiter, current_process);
-
-    device_request_io(dev, &req);
-
-    proc_block(current_process);
-
-    return req.offset;
-}
-
-ssize_t device_read(struct device* dev, void* buffer, size_t count, off_t offset)
-{
-    struct io_request req = {
-        .buffer = buffer,
-        .offset = offset,
-        .count = count,
-        .op = 0
-    };
-    waiter_push(&req.waiter, current_process);
-
-    device_request_io(dev, &req);
-
-    proc_block(current_process);
-    
-    return req.offset;
-}
-
-//device lookup is O(n) because devices are kept as linked lists
 struct device* device_lookup(dev_t devno)
 {
     struct device_driver* drv = driver_table[MAJOR(devno)];
-    if (!drv) {
-        return NULL;
-    }
-
-    struct device* dev = drv->instances;
-    while(dev) {
+    for (struct device* dev = drv->instances; dev; dev = dev->next) {
         if (dev->minor == MINOR(devno)) {
             return dev;
         }
-        dev = dev->next;
     }
     return NULL;
 }
 
-void device_global_update()
-{
-    for (int i = 0; i < DRIVER_TABLE_LEN; i++) {
-        struct device_driver* drv = driver_table[i];
-        if (!drv) {
-            continue;
-        }
-        struct device* current = drv->instances;
-        while(current) {
-            drv->update(current);
-            current = current->next;
-        }
-    }
-}
 
